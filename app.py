31f186a366ff96df49a667d6e8ab209c9175292e
kalidindiDivya <kalidindidivya632@gmail.com>
2025-12-17 11:42:31 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+
+st.markdown("<h1 style='color:green'>BVC College App</h1>", unsafe_allow_html=True)
+st.write("Welcome to the BVC College App.")
+st.image("clg.png.jpg", caption="BVC College Campus")
